0c214d5ac2f6f62d2b633f109be714667d612fa5
bp2881 <pranavbairy2@gmail.com>
2024-05-18 19:00:36 +0530

    conda in ubuntu

--- test3.py
+++ test3.py
@@ -1,4 +1,11 @@
-import os
- 
-print('File name :    ', os.path.basename(__file__))
-print('Directory Name:     ', os.path.dirname(__file__))
+import requests
+from bs4 import BeautifulSoup
+from os import system
+
+r = requests.get("https://repo.anaconda.com/archive/")
+content = BeautifulSoup(r.content, "html.parser")
+s = content.find('div', class_='entry-content')
+
+system("rm -rf archive.txt")
+with open("archive.txt", "w+") as a:
+    a.write(str(content))
